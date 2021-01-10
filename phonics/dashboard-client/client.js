4eea6d8d8e4c4ece793bee04dd58c4212713c964
Tom Roman <tom@tomroman.co.uk>
2021-01-10 00:20:35 +0000

    Boom Dashboard Connecting

--- phonics/dashboard-client/client.js
+++ phonics/dashboard-client/client.js
@@ -1,11 +1,11 @@
 const WebSocket = require('ws');
 const logger = require('../utils/logger')
 
-const ws = new WebSocket('');
+const ws = new WebSocket('ws://pal-jfk2:7892');
 logger.clientLog("Connected to Dashboard")
 
 ws.on('open', function open() {
-  ws.send('test');
+  ws.send(JSON.stringify({"id":22,"type":"audioserver"}));
 });
 
 ws.on('message', function incoming(data) {
