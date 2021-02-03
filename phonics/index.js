66c1faabe95183d636876ee8f72e6bec5da5ecda
Tom Roman <tom@tomroman.co.uk>
2021-02-03 00:04:57 +0000

    Add consumer start to index.js

--- phonics/index.js
+++ phonics/index.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 const logger = require("./utils/logger");
 var pjson = require("./package.json");
 var dashClient = require("./dashboard-client/index");
+var mqConsumer = require("./rabbit-mq-consumer/index");
 var playerMap = new Map();
 
 logger.primaryLog("Starting Phonics v" + pjson.version);
 
 if (!process.env.DASH_LEGACY) {
-    
+    mqConsumer.startConsumer();
 } else {
     logger.warningLog("Phonics is running with Dashboard legacy mode! Did you mean to do this?")
     logger.warningLog("Halting for 5 seconds before booting")
